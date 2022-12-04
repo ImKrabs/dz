41aecbacb7a63fa9bb8842deaf47c8e80d3ffcfd
sas16052010 <77286807+sas16052010@users.noreply.github.com>
2022-12-04 19:39:47 +0200

    dz 1

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-print('ok')
-print("geg")
-print("deh")
+class Cat:
+    view = 'domestic'
+    species = "smooth-haired"
+
+    def __init__(self, species, age, color):
+        self.species = species
+        self.age = age
+        self.color = color
+        print('Создался объект')
+
+
+
+an1 = Cat(species='Asian', age=7, color="grey")
+an2 = Cat(species='European', age=10, color="ginger")
+
+print(an1.species)
+print(an1.age)
+print(an1.color)
+print(an2.species)
+print(an2.age)
+print(an2.color)
+
+
+
